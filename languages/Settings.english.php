12a38af72f194fc146634d40dc9c28cb7b782a55
Diego Andrés <diegoandres_cortes@outlook.com>
2021-11-04 17:32:01 -0600

    gh release
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/Settings.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.0 RC5; Settings
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Crushing.<br /><br />Author: <a target="_blank" href="http://smftricks.com">Diego Andr&eacute;s</a>';
+
+?>
---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,10 @@
+<?php
+
+$txt['lgf_time'] = 'Time';
+$txt['link_top'] = 'The title of your link';
+$txt['link_top_desc'] = 'This links appears above your forum, in the top bar.';
+$txt['link_top_url'] = 'The url of your title';
+$txt['link_top1'] = 'Link 1';
+$txt['link_top2'] = 'Link 2';
+$txt['link_top3'] = 'Link 3';
+?>
