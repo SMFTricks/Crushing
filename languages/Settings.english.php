9d1ca14587faf3460e5d4811c7ed5924e2eef5f9
Diego Andrés <diegoandres_cortes@outlook.com>
2021-11-04 18:04:49 -0600

    fix copyright
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- languages/Settings.english.php
+++ languages/Settings.english.php
@@ -4,6 +4,6 @@
 global $settings;
 
 $txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
-$txt['theme_description'] = 'Crushing.<br /><br />Author: <a target="_blank" href="http://smftricks.com">Diego Andr&eacute;s</a>';
+$txt['theme_description'] = 'Crushing Theme.<br /><br />Author: <a href="https://smftricks.com">SMF Tricks Team</a>';
 
 ?>
